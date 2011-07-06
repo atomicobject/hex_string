2fbc1add8057e366b477a74a6a027a79cca769aa
David Crosby <dcrosby42@gmail.com>
2011-07-05 23:26:29 -0400

    Inline documentation

--- lib/hex_string.rb
+++ lib/hex_string.rb
@@ -1,4 +1,18 @@
+# String extensions providing to_hex_string and to_byte_string.
+# Requiring hex_string will automatically include HexString in the String class.
 module HexString
+
+  # Convert a human-readable hex string into binary data.
+  #
+  # Assumes the target String is an ASCII, human readable sequence of hexadecimal 
+  # tuples depicting a sequence of 8-bit byte values.
+  # Whitespace between tuples is allowed and will be removed before packing.
+  #
+  # Returns a newly created string containing binary data -- the target string will not be modified.
+  #
+  # Eg:
+  #   >> "68 65 6c 6c 6f".to\_byte\_string
+  #   => "hello"
 	def to_byte_string
 		stripped = self.gsub(/\s+/,'')
 		unless stripped.size % 2 == 0
@@ -8,11 +22,22 @@ module HexString
     [stripped].pack('H*')
 	end
 
+  # Convert binary data into a human-readable hex string.
+  #
+  # Whatever data is contained in the target string will be "exploded" into a sequence of 
+  # hexadecimal tuples, one space between each tuple, for ease of debugging and reading.
+  # 
+  # Returns a newly created string containing the hex string -- the target binary string will not be modified.
+  #
+  # Eg:
+  #   >> "hello world".to\_hex\_string
+  #   => "68 65 6c 6c 6f 20 77 6f 72 6c 64"
 	def to_hex_string
     self.unpack('H*').first.gsub(/(..)/,'\1 ').rstrip
 	end
 end
 
+# Include HexString extensions in the String class
 class String
 	include HexString
 end
